aa757a5afc43b445c96e5b3dc2a463c28247eda1
UchithChethana <it22187796@my.sliit.lk>
2024-08-27 16:38:25 +0530

    test

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 import Footer from './Components/Footer/Footer';
 import Navbar from './Components/ui/Navbar';
 
+
 function App() {
   return (
    <div>
     <Navbar/>
 
     <h1>Staff management</h1>
+
 
    <Footer/>
 
